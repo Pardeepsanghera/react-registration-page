e89d8dd5ba2177aacb3dacd45258e30044a92965
Pardeep sanghera <pardeepsanghera@Pardeeps-MacBook-Pro.local>
2021-05-20 16:57:43 -0700

    feat(navbar) Added a navbar and Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div></div>
-    )
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      {/* Logo */}
+      <Link to='/' className='navbar-brand'>
+        Home
+      </Link>
+
+      {/* Menu */}
+      <div className='collapse navbar-collapse'>
+        <div className='navbar-nav'>
+          <Link className='nav-link' to='/'>
+            Registration
+          </Link>
+        </div>
+      </div>
+
+    </nav>
+  );
 };
 
 export default Navbar;
